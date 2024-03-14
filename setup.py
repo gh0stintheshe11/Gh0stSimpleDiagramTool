6eee5f942bdf553bc714115125f74e5bcc65bd05
gh0stintheshe11 <langs.971104@gmail.com>
2024-03-14 02:59:57 -0400

    update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text(encoding='utf-8')
 
 setup(
     name="gh0st-SimpleDiagramTool",
-    version="0.1",
+    version="0.0.1",
     packages=find_packages(),
     install_requires=[
         "matplotlib",
