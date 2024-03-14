1b53f9b7f0bf4fd3d0b214bf7ca7607004ccdb87
gh0stintheshe11 <77403731+gh0stintheshe11@users.noreply.github.com>
2024-03-14 00:31:59 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "matplotlib",  # Add any other dependencies required by your library
+        "matplotlib",
     ],
     author="gh0stintheshe11",
     author_email="gh0stintheshe11@gmail.com",
     description="A simple diagram drawing tool",
     license="MIT",
     keywords="diagram flowchart matplotlib",
-    url="https://github.com/gh0stintheshe11/gh0st-SImpleDiagramTool",  # Replace with your actual GitHub URL
+    url="https://github.com/gh0stintheshe11/gh0st-SImpleDiagramTool",
 )
