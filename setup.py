0404a26b1b2cbdb2fb398228de337d805b8da234
gh0stintheshe11 <langs.971104@gmail.com>
2024-03-14 00:13:35 -0400

    update

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="gh0st_Simple_Diagram_Tool",
+    name="gh0st-SimpleDiagramTool",
     version="0.1",
     packages=find_packages(),
     install_requires=[
