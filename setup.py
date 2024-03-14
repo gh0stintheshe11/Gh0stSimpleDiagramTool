54af4a8b581f2bad8955ffc93f7b018ec2fad23e
gh0stintheshe11 <langs.971104@gmail.com>
2024-03-14 02:06:32 -0400

    update

--- setup.py
+++ setup.py
@@ -6,5 +6,18 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text(encoding='utf-8')
 
 setup(
-    name="gh0st-SimpleDiagramTool",version="0.1",packages=find_packages(),install_requires=["matplotlib",], author="gh0stintheshe11", author_email="gh0stintheshe11@gmail.com", description="A simple diagram drawing tool", long_description=long_description, long_description_content_type='text/markdown', license="MIT", keywords="diagram flowchart matplotlib", url="https://github.com/gh0stintheshe11/gh0st-SImpleDiagramTool",
+    name="gh0st-SimpleDiagramTool",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "matplotlib",
+    ],
+    author="gh0stintheshe11",
+    author_email="gh0stintheshe11@gmail.com",
+    description="A simple diagram drawing tool",
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important for Markdown
+    license="MIT",
+    keywords="diagram flowchart matplotlib",
+    url="https://github.com/gh0stintheshe11/gh0st-SImpleDiagramTool",
 )
