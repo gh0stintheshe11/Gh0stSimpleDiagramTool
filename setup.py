c8cc332d5bf8a009da6a7a547d8842dc85169620
gh0stintheshe11 <77403731+gh0stintheshe11@users.noreply.github.com>
2024-03-14 00:51:11 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup, find_packages
 
+# Read the contents of your README file
+with open('README.md', encoding='utf-8') as f:
+    long_description = f.read()
+
 setup(
     name="gh0st-SimpleDiagramTool",
     version="0.1",
@@ -10,6 +14,8 @@ setup(
     author="gh0stintheshe11",
     author_email="gh0stintheshe11@gmail.com",
     description="A simple diagram drawing tool",
+    long_description=long_description,
+    long_description_content_type='text/markdown',  # This is important for Markdown
     license="MIT",
     keywords="diagram flowchart matplotlib",
     url="https://github.com/gh0stintheshe11/gh0st-SImpleDiagramTool",
