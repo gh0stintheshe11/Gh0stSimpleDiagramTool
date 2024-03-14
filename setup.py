2a78a74e5c07c8f582442c5abda3704ed3ddbdc2
gh0stintheshe11 <langs.971104@gmail.com>
2024-03-14 01:54:25 -0400

    update

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup, find_packages
 # read the contents of your README file
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+long_description = (this_directory / "README.md").read_text(encoding='utf-8')
 
 setup(
     name="gh0st-SimpleDiagramTool",
-    version="1.0.0",
+    version="0.1",
     packages=find_packages(),
     install_requires=[
         "matplotlib",
